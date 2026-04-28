81cceac427d5b5b161ce2a8f4d1076a52f86c7a8
Giovanna 🌙 <giovanna.s.almeida8@aluno.senai.br>
2026-04-28 11:05:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'sorveteria-docil';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/images/logo-sorvete.webp',
+    './assets/images/logo-celular.png',
+    './assets/images/logo-512.png',
+    './assets/images/sorvete.webp',
+    './assets/images/whatsapp.webp',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Fazendo Sorvete no Cache! 🍦');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
